40ff93e5f2b686e9d79fcdfa5add04b4a962ba08
SakthikumarGurunathan <sakthikumargurunathan@gmail.com>
2023-03-21 22:25:12 +0530

    updated projects

--- src/Projects/Projects.jsx
+++ src/Projects/Projects.jsx
@@ -14,23 +14,34 @@ export default function Projects({jp}){
         <a href="https://tenziezz.netlify.app/" target={"_blank"}><img src="tenzies.png" alt="tnzies-img" /></a>
     </div>
 
+    <div class="samples" data-aos="fade-up" data-aos-duration="2000">
+    <a href="https://nileshopping.netlify.app/" target={"_blank"}><img src="Shopping.png" alt="shopping-img" /></a>
+        <div>
+            <h1 className="project-title">{jp ? "ショッピングサイト":"Shopping site"}</h1>
+            <h4>{jp? "シンプルな通販サイトです" :"This is a simple shopping website"}</h4>
+            <h4>{jp?"これは、React ルーターを使用して実装された単一ページ アプリケーションです。":"This is a single page application implemented using React router."}</h4>
+            <h4>{jp?"このユーザーでは、製品の詳細を見ることができます。":"In this user can see the details of the products."}</h4>
+        </div>
+       
+    </div>
 
         <div class="samples"  data-aos="fade-up" data-aos-duration="2000">
             <div class="samples" data-aos="fade-up" data-aos-duration="2000">
-                 <a href="https://incomparable-centaur-f28a48.netlify.app" target={"_blank"}><img src="/cardwar.png" alt="card-war-project-img"/></a>
             <div>
             <h1 className="project-title"> {jp ? "戦争のカード":"Card of war"}</h1>
             <h4>{jp ? "パソコンと遊べる簡単なカードゲームで、最終的にポイントが高い人が勝者となります" : "This is a simple card game that you can play with computer, the winner of the game will be decided at the end whoever has the highest point"}</h4></div>
         </div>
+        <a href="https://incomparable-centaur-f28a48.netlify.app" target={"_blank"}><img src="/cardwar.png" alt="card-war-project-img"/></a>
         </div>
 
         <div class="samples" data-aos="fade-up" data-aos-duration="2000">
+        <a href="https://colorschememaker.netlify.app" target={"_blank"}><img src="color.png" alt="color-scheme-img" /></a>
         <div>
             <h1 className="project-title">{jp ? "カラースキーム":"Color scheme"}</h1>
             <h4>{jp? "このプロジェクトは、プロジェクトで配色が必要なときにいつでも使用できます" :"This project can be used whenever color schemes are needed for our projects"}</h4>
             <h4>{jp?"ユーザーが必要な色とモーダルを選択すると、API からデータが取得されます":"It get the data from the api as user choose the color and the modal they needed"}</h4>
         </div>
-        <a href="https://colorschememaker.netlify.app" target={"_blank"}><img src="color.png" alt="color-scheme-img" /></a>
+       
     </div>
 
 
